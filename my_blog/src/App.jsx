b5d4d8345c498aecf40e7755923a162d524ba4b6
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-10-25 22:24:31 +0300

    Static html and css whith Tailwind has been completed

--- my_blog/src/App.jsx
+++ my_blog/src/App.jsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+
+
 import Sidebar from './components/sidebar'
+import Header from './components/Header'
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Home />,
+    // element: <Layout />,
+    // children: [
+    //   { index: true, element: <Home /> },
+    //   { path: "old-home", element: <Navigate to={"/"} /> },
+    //   { path: "about", element: <About /> },
+    //   { path: "cart", element: <Cart /> },
+    //   { path: "thanks", element: <Thanks /> },
+    //   { path: "category/:categoryId", element: <Category /> },
+    //   { path: "product/:productId", element: <ProductDetails /> },
+    //   { path: "*", element: <NotFound /> },
+      // { path: "*", element: <Navigate to="/" /> },
+    // ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <Sidebar/>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ my_blog/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+
+function Header() {
+    return (
+        <nav className="p-4 bg-[#0d0d0dff] shadow-header">
+            <ul className="flex flex-row gap-6">
+                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                    <NavLink>
+                        Главное
+                    </NavLink>
+                </li>
+                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                    <NavLink>
+                        Обо мне
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
