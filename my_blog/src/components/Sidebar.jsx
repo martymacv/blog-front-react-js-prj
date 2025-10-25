514255cd165a01491300bc4ab9525aba5a30e55c
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-10-25 21:59:58 +0300

    Static html and css whith Tailwind has been completed

--- my_blog/src/components/Sidebar.jsx
+++ my_blog/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
     return (
-        <sidebar className="flex flex-col gap-4 w-xs bg-[#202020ff] items-center justify-center">
+        <sidebar className="flex flex-col shadow-sidebar gap-4 w-xs bg-[#202020ff] items-center justify-center">
             <div className='relative flex flex-col items-center justify-start w-full h-[230px] mb-3'>
                 <img className='w-full' src="src/assets/images/wallpaper.png" alt="wallpaper" />
                 <div className='absolute rounded-full border-2 border-white top-[140px]'>
@@ -10,19 +10,19 @@ export default function Sidebar() {
                 </div>
             </div>
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='text-white text-2xl'>Ролан Закиров</h1>
-                <p className='text-white'>Front-end разработчик</p>
+                <h1 className='text-white text-[18px] font-[400] font-roboto'>Ролан Закиров</h1>
+                <p className='text-[#dededeff] text-[14px] font-[300] font-roboto'>Front-end разработчик</p>
             </div>
             <div className='flex flex-row gap-4 items-center justify-center pb-3'>
                 <img src="src/assets/images/instagram.svg" alt="instagram" />
                 <img src="src/assets/images/vk.svg" alt="vk" />
             </div>
             <hr className='border-1 w-[260px] border-[#1c1c1cff]' />
-            <p className='text-white text-center py-3 px-5'>Front-end разработчик. Практик верстки сайтов. Созданием сайтов занимаюсь с 2012 года. Работал в нескольких ИТ компаниях и наработал более 10 000 часов в создании сайтов различной сложности.</p>
+            <p className='text-white text-[12px] font-[400] leading-[18px] tracking-[0%] text-center py-3 px-5 font-roboto'>Front-end разработчик. Практик верстки сайтов. Созданием сайтов занимаюсь с 2012 года. Работал в нескольких ИТ компаниях и наработал более 10 000 часов в создании сайтов различной сложности.</p>
             <hr className='border-1 w-[260px] border-[#1c1c1cff]' />
             <div className='flex flex-row gap-5 items-center justify-center p-4'>
-                <button className='text-white bg-[#ed3024ff] rounded-full w-[120px] h-[40px]' type='button'>Мои работы</button>
-                <button className='text-white bg-[#3137c9ff] rounded-full w-[120px] h-[40px]' type='button'>Написать мне</button>
+                <button className='text-white font-roboto text-button bg-[#ed3024ff] rounded-full w-[120px] h-[40px]' type='button'>Мои работы</button>
+                <button className='text-white font-roboto text-button bg-[#3137c9ff] rounded-full w-[120px] h-[40px]' type='button'>Написать мне</button>
             </div>
         </sidebar>
     );
