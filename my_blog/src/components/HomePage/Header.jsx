934bc395eb5f071c982c2cf6b83d3c08ab23dd60
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-10-27 23:10:58 +0300

    Modified Header.jsx

--- my_blog/src/components/HomePage/Header.jsx
+++ my_blog/src/components/HomePage/Header.jsx
@@ -1,31 +1,46 @@
 import { NavLink } from "react-router-dom"
+import Input from "../generals/Input"
 
 function Header() {
     return (
-        <nav className="w-full p-4 bg-[#0d0d0dff] shadow-header">
-            <ul className="flex flex-row gap-6">
-                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
-                    <NavLink>
-                        Главное
-                    </NavLink>
-                </li>
-                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
-                    <NavLink>
-                        Обо мне
-                    </NavLink>
-                </li>
-                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
-                    <NavLink to={"/profile/1"}>
-                        Профиль
-                    </NavLink>
-                </li>
-                <li className="text-white text-[11px] font-[400] font-roboto uppercase">
-                    <NavLink to={"/login"}>
-                        Войти
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+        <div className="flex flex-row justify-between items-center p-4 bg-[#0d0d0dff] shadow-header">
+            <nav className="">
+                <ul className="flex flex-row flex-wrap gap-6">
+                    <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                        <NavLink>
+                            Главное
+                        </NavLink>
+                    </li>
+                    <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                        <NavLink>
+                            Обо мне
+                        </NavLink>
+                    </li>
+                    <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                        <NavLink to={"/profile/1"}>
+                            Профиль
+                        </NavLink>
+                    </li>
+                    <li className="text-white text-[11px] font-[400] font-roboto uppercase">
+                        <NavLink to={"/login"}>
+                            Войти
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <NavLink 
+                className="text-white text-[11px] font-[400] font-roboto uppercase">
+                Поиск
+            </NavLink>
+            <form className="min-w-90 mx-2 hidden"
+                action="POST">
+                <Input
+                    type="text"
+                    id="search"
+                    name="search"
+                    placeholder="Поиск по блогу"></Input>
+            </form>
+        </div>
     )
 }
 
